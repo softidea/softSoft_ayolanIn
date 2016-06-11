cc4ab47084758217540abe58de5985264890fada
deepalsuranga <deepalsuranga@live.com>
2016-06-11 05:39:37 +0530

    login and logout

---
+++ Aylan_java/src/account/instralament.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package account;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author deepalsuranga
+ */
+public class instralament {
+
+    public static void main(String[] args) {
+        instralament a = new instralament();
+        a.instralamentfull(40000.0, 10.0, 5.0);
+    }
+
+    public double instralamentfull(double dena_wt, double masa_ganana, double masekata_poliya) {
+
+        double ful = dena_wt * masekata_poliya / 100;
+        System.out.println(ful);
+        double full = dena_wt / masa_ganana;
+        System.out.println(full);
+        double fully = ful + full;
+        System.out.println(fully);
+        return fully;
+
+    }
+
+    public double instralament(double dena_wt, double masa_ganana, double masekata_poliya) {
+
+        double C = dena_wt;
+        double t = masa_ganana;
+        double Ps;
+        double r = masekata_poliya;
+        double a = (1 + r);
+        Math.pow(a, t);
+        Ps = C * a * t;
+        System.out.println("warikaya :" + Ps);
+
+        return Ps;
+
+    }
+
+}
---
+++ Aylan_java/src/com/softidea/www/public_access/user/login/User_SendMailSSL.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.softidea.www.public_access.user.login;
+
+/**
+ *
+ * @author deepalsuranga
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class User_SendMailSSL {
+    
+    String system_mail;
+    String system_mail_password;
+
+    public void sendingSSL(String to_mail, String subject_plus, String message_body) {
+
+        String to = to_mail;
+                
+                //"acedamy_manager@outlook.com";//change accordingly
+        
+        ///////////////////////////////////////////////////////////////////////
+        
+        //////////////////////////////////////////////////////////////////////
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("src\\Softidea_Production\\db_pro.properties");
+
+            
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("SYSTEM_MAIL"));
+            System.out.println(prop.getProperty("SYSTEM_PASSWORD"));
+            //System.out.println(prop.getProperty("SYSTEM_PORT"));
+            System.out.println(prop.getProperty("SYSTEM_ADMIN_RC"));
+            
+            
+            
+            system_mail = prop.getProperty("SYSTEM_MAIL");
+            system_mail_password = prop.getProperty("SYSTEM_PASSWORD");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        ////////////////////////////////////////////////////////////////////
+        
+        ////////////////////////////////////////////////////////////////////////
+
+        //Get the session object
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(system_mail, system_mail_password);//change accordingly
+                    }
+                });
+
+//compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(system_mail));//change accordingly
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("System Login Alert:" + subject_plus);
+            message.setText(message_body);
+
+            //send message
+            Transport.send(message);
+
+            System.out.println("message sent successfully");
+
+        } catch (MessagingException e) {
+            
+            e.printStackTrace();
+        }
+
+    }
+
+    
+}
